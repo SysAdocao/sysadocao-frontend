1579e9c561f72c1a63049b2f3954bca4a0a5336f
Fernando Lima <eufernandoprogramador@gmail.com>
2024-11-22 18:50:02 -0300

    feat: atualizando header para usuário autenticado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,78 @@
-import AppBar from "@mui/material/AppBar";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { useAppThemeContext } from "../contexts/ThemeContext";
+
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import Logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
-import { useAppThemeContext } from "../contexts/ThemeContext";
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
 
-const navItems = [{name: "Cadastrar", route: "/sign-up"}, {name: "Entrar", route: "/sign-in"}];
+import Logo from "../assets/logo.png";
+import { Icon } from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Avatar from "@mui/material/Avatar";
 
 function Header() {
-  const { themeName, toggleTheme } = useAppThemeContext()
-  
+  const { isLogged, userName, handleLogout } = useAuth();
+  const { themeName, toggleTheme } = useAppThemeContext();
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar component="nav" sx={{py: 1, px: 6}}>
+      <AppBar component="nav" sx={{ py: 1, px: 6 }}>
         <Toolbar>
           <Box sx={{ flexGrow: 1 }}>
-            <Link to={"/"}>
+            <Link to="/">
               <img src={Logo} alt="Logo SysAdoção" />
             </Link>
           </Box>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Link to={item.route} key={item.name}>
-                <Button sx={{ color: "white" }}>
-                  {item.name}
-                </Button>
-              </Link>
-            ))}
+          <Box
+            sx={{
+              alignItems: "center",
+              gap: 2,
+              display: { xs: "none", sm: "flex" }
+            }}
+          >
+            {isLogged ? (
+              <>
+                <Icon sx={{ cursor: "pointer" }}>
+                  <FavoriteIcon />
+                </Icon>
+                <Icon sx={{ cursor: "pointer" }}>
+                  <NotificationsIcon />
+                </Icon>
+                <Icon
+                  sx={{ cursor: "pointer" }}
+                  color="white"
+                  onClick={toggleTheme}
+                >
+                  {themeName === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+                </Icon>
+                <Avatar sx={{ cursor: "pointer" }} onClick={handleLogout}>
+                  {userName
+                    .split(" ")
+                    .slice(0, 2)
+                    .map((name) => name[0])
+                    .join("")
+                    .toUpperCase()}
+                </Avatar>
+              </>
+            ) : (
+              <>
+                <Link to={"/sign-up"}>
+                  <Button sx={{ color: "white" }}>Cadastrar</Button>
+                </Link>
+                <Link to={"/sign-in"}>
+                  <Button sx={{ color: "white" }}>Entrar</Button>
+                </Link>
+              </>
+              
+            )}
           </Box>
-          <Button color="white" onClick={toggleTheme}>{themeName === "light" ? <DarkModeIcon /> : <LightModeIcon />}</Button>
         </Toolbar>
       </AppBar>
     </Box>
